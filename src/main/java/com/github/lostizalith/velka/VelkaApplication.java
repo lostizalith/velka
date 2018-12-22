4fc0813207944cfaf409219fbb355c0781359b22
solairerove <solairerove@gmail.com>
2018-12-22 22:35:21 +0300

    feature: initial commit
    create simple greeting application

---
+++ src/main/java/com/github/lostizalith/velka/VelkaApplication.java
@@ -0,0 +1,12 @@
+package com.github.lostizalith.velka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VelkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VelkaApplication.class, args);
+	}
+}
