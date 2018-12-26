bcd1679e022ceaec9159754c00dfab2ba7e7b38d
solairerove <solairerove@gmail.com>
2018-12-26 16:28:12 +0300

    #11: Migrate to module system

--- src/main/java/com/github/lostizalith/velka/record/Record.java
+++ src/main/java/com/github/lostizalith/velka/record/Record.java
@@ -1,13 +1,50 @@
 package com.github.lostizalith.velka.record;
 
-import lombok.Data;
-
-@Data
 public class Record {
 
     private String id;
     private String memo;
     private String date;
     private String income;
-    private String expense; 
+    private String expense;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMemo() {
+        return memo;
+    }
+
+    public void setMemo(String memo) {
+        this.memo = memo;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getIncome() {
+        return income;
+    }
+
+    public void setIncome(String income) {
+        this.income = income;
+    }
+
+    public String getExpense() {
+        return expense;
+    }
+
+    public void setExpense(String expense) {
+        this.expense = expense;
+    }
 }
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module velka {
+    exports com.github.lostizalith.velka;
+
+    requires spring.boot.autoconfigure;
+    requires spring.boot;
+    requires spring.web;
+    requires lombok;
+}
