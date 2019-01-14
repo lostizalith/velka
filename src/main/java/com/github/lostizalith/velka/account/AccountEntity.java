3c565f5171b66bd140707e4ca7e657441cdf5d5a
Mikita Kryvitski <solairerove@gmail.com>
2019-01-14 22:18:14 +0300

    #4: set up db integration for account and record

--- src/main/java/com/github/lostizalith/velka/account/AccountEntity.java
+++ src/main/java/com/github/lostizalith/velka/account/AccountEntity.java
@@ -2,28 +2,75 @@ package com.github.lostizalith.velka.account;
 
 import com.github.lostizalith.velka.record.RecordEntity;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.UUID;
 
 @Data
+@Entity
+@Table(name = "account")
 public class AccountEntity {
 
-    private UUID id = UUID.randomUUID();
+    @EqualsAndHashCode.Exclude
+    @Id
+    @Column(name = "a_id", nullable = false)
+    private UUID id;
 
+    @Size(max = 64)
+    @Column(name = "a_display_name")
     private String displayName;
 
+    @Size(max = 256)
+    @Column(name = "a_short_description")
     private String shortDescription;
 
+    @NotNull
+    @Column(name = "a_type")
+    @Enumerated(EnumType.STRING)
     private AccountType accountType;
 
+    @Column(name = "a_current_balance")
     private Double currentBalance;
 
+    @NotNull
+    @Column(name = "a_currency")
+    @Enumerated(EnumType.STRING)
     private AccountCurrency currency;
 
+    @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    @OneToMany(mappedBy = "account", cascade = CascadeType.ALL, orphanRemoval = true)
+    @Fetch(FetchMode.SELECT)
     private List<RecordEntity> records;
 
-    // TODO: CashFlow?
+    @EqualsAndHashCode.Exclude
+    @CreatedDate
+    @Column(name = "a_created", updatable = false)
+    private LocalDateTime created;
 
-    // TODO: accounter name
+    @EqualsAndHashCode.Exclude
+    @LastModifiedDate
+    @Column(name = "a_modified")
+    private LocalDateTime modified;
+
+    @EqualsAndHashCode.Exclude
+    @Column(name = "a_deactivated", updatable = false, insertable = false)
+    private LocalDateTime deactivated;
 }
--- src/main/java/com/github/lostizalith/velka/record/RecordEntity.java
+++ src/main/java/com/github/lostizalith/velka/record/RecordEntity.java
@@ -1,18 +1,25 @@
 package com.github.lostizalith.velka.record;
 
+import com.github.lostizalith.velka.account.AccountEntity;
 import com.github.lostizalith.velka.category.InternalCategoryEntity;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -24,33 +31,42 @@ public class RecordEntity {
 
     @EqualsAndHashCode.Exclude
     @Id
-    @Column(name = "record_id", nullable = false)
+    @Column(name = "r_id", nullable = false)
     private UUID id;
 
     @Size(max = 256)
-    @Column(name = "record_memo")
+    @Column(name = "r_memo")
     private String memo;
 
-    // TODO: record type instead and some field like value
-    private String income;
+    @NotNull
+    @Column(name = "r_type")
+    @Enumerated(EnumType.STRING)
+    private RecordType recordType;
 
-    private String expense;
+    @Column(name = "r_flow")
+    private Double flow;
 
     @OneToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE, CascadeType.REFRESH, CascadeType.DETACH})
-    @JoinColumn(name = "int_category_id")
+    @JoinColumn(name = "ic_id")
     private InternalCategoryEntity internalCategory;
 
     @EqualsAndHashCode.Exclude
+    @ToString.Exclude
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "a_id")
+    private AccountEntity account;
+
+    @EqualsAndHashCode.Exclude
     @CreatedDate
-    @Column(name = "record_created", updatable = false)
+    @Column(name = "r_created", updatable = false)
     private LocalDateTime created;
 
     @EqualsAndHashCode.Exclude
     @LastModifiedDate
-    @Column(name = "record_modified")
+    @Column(name = "r_modified")
     private LocalDateTime modified;
 
     @EqualsAndHashCode.Exclude
-    @Column(name = "record_deactivated", updatable = false, insertable = false)
+    @Column(name = "r_deactivated", updatable = false, insertable = false)
     private LocalDateTime deactivated;
 }
